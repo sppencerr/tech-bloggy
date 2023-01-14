8313629d4fc62ad914819b43e0bd09ee5009353a
Spencer Gerritsen <spencergerritsen@gmail.com>
2023-01-14 14:57:23 -0700

    index.js in models

--- models/index.js
+++ models/index.js
@@ -4,34 +4,39 @@ const Comment = require("./Comment");
 
 User.hasMany(Post, {
   foreignKey: "userId",
-  onDelete: "CASCADE"
+  onDelete: "CASCADE",
+  as: "posts",
 });
 
 User.hasMany(Comment, {
   foreignKey: "userId",
-  onDelete: "CASCADE"
+  onDelete: "CASCADE",
+  as: "comments",
 });
 
 Post.belongsTo(User, {
   foreignKey: "userId",
-  onDelete: "CASCADE"
+  onDelete: "CASCADE",
+  as: "author",
 });
 
 Post.hasMany(Comment, {
   foreignKey: "postId",
-  onDelete: "CASCADE"
+  onDelete: "CASCADE",
+  as: "comments",
 });
 
 Comment.belongsTo(User, {
-  foreignKey: "userId"
+  foreignKey: "userId",
+  as: "author",
 });
 
 Comment.belongsTo(Post, {
-  foreignKey: "postId"
+  foreignKey: "postId",
+  as: "post",
 });
-
 module.exports = {
   User,
   Comment,
-  Post
+  Post,
 };
