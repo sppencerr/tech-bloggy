6b7027c308e8081ab006bd0fa748fdd285a343c3
Spencer Gerritsen <spencergerritsen@gmail.com>
2023-01-15 00:14:48 -0700

    handlebars, routes, tailwind

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const { Comment } = require('../../models/');
+const withAuth = require('../../utils/auth');
+
+router
+  .route('/')
+  .get(withAuth, async (req, res) => {
+    try {
+      const commentData = await Comment.findAll({
+        include: [User],
+      });
+      const comments = commentData.map((comment) => comment.get({ plain: true }));
+      res.render('single-post', { comments, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  })
+  .post(withAuth, async (req, res) => {
+    const body = req.body;
+    try {
+      const newComment = await Comment.create({
+        ...body,
+        userId: req.session.userId,
+      });
+      res.json(newComment);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('expresss');
+const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const exphbs = require('express-handlebars');
